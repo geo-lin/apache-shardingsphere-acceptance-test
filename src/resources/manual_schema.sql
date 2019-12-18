717a4afb1b128faaf129cdd3f5611fc20fe31744
wanghiato71 <haetao@outlook.com>
2019-12-18 10:55:07 +0800

    modify docker-compose

--- src/resources/manual_schema.sql
+++ src/resources/manual_schema.sql
@@ -1,3 +1,5 @@
+set GLOBAL max_connections = 600;
+
 DROP SCHEMA IF EXISTS demo_ds;
 DROP SCHEMA IF EXISTS demo_ds_0;
 DROP SCHEMA IF EXISTS demo_ds_1;
@@ -27,8 +29,6 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_master_0_slave_1;
 CREATE SCHEMA IF NOT EXISTS demo_ds_master_1;
 CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_0;
 CREATE SCHEMA IF NOT EXISTS demo_ds_master_1_slave_1;
-
-set GLOBAL max_connections = 600;
 
 -- Should sync from master-salve automatically
 -- CREATE TABLE IF NOT EXISTS demo_ds_slave_0.t_order (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
